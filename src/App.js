86efc77f250a590b3d52f6cfa28727e7023e2b5d
tracymoige <tracymoige@outlook.com>
2023-05-23 23:14:56 +0300

    Added weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
       </div>
     </div>
   );
