f832e13ac9d4a81bcd7a28c7efdb3843cd6a646c
tracymoige <tracymoige@outlook.com>
2023-05-25 16:42:21 +0300

    Styled the CSS

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather />
+        <p className="mt-3">
+          This project was coded by Tracy Moige and is{" "}
+          <a href="https://github.com/tracymoige/react-weather-project">
+            open sourced on Github
+          </a>
+        </p>
       </div>
     </div>
   );
