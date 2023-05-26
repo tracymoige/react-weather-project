8af07a42b4055590c5ae692fef57fbd35686ecd4
tracymoige <tracymoige@outlook.com>
2023-05-26 13:11:18 +0300

    Updated links

--- src/App.js
+++ src/App.js
@@ -8,9 +8,22 @@ export default function App() {
         <Weather defaultCity="Nairobi" />
         <p className="mt-3">
           This project was coded by Tracy Moige and is{" "}
-          <a href="https://github.com/tracymoige/react-weather-project">
+          <a
+            href="https://github.com/tracymoige/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
             open sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://thunderous-cat-c4359e.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
+          .
         </p>
       </div>
     </div>
